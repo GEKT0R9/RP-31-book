316340857254f71784b4a34b81953ccaa5784a22
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2020-02-06 22:02:09 +0400

    Update index.php

--- RP-31-book/index.php
+++ RP-31-book/index.php
@@ -1,7 +1,9 @@
 <?php
+//Подключение фона (background)
 include('BG.php');
 //подключение бд
 require('connect_bd.php');
+//Работа с HTML формой
 echo '<div><table class="form">';
 echo '<button class="icon-btn add-btn">
         <div class="add-icon"></div>
@@ -17,13 +19,13 @@ echo '<button class="icon-btn add-btn">
       <button  class="icon-btn add-btn">
         <div class="btn-txt"><a href="form/deletewriter.php">Удалить автора</a></div>
       </button>';
-//echo '<a href="form/addbook.php"></a>
-//      <a href="form/delete.php"></a>';
-
+//Отправка запроса в БД и запись результата в переменную
 $sql = $dbc->query('SELECT books.*, authors.* FROM books,authors,books_authors WHERE books_authors.id_books = books.id AND books_authors.id_author = authors.id');
+//Преобразование результата запроса в массив
 $sql = $sql->fetchAll();
-
+//Работа с таблицей
 echo '<tr class="tr"><td>Название</td><td>Цена</td><td>Автор</td></tr>';
+//Запись данных в таблицу
 foreach ($sql as $row) {
     echo '<tr class="tr">';
     echo '<td>' . $row['title'] . '</td>';
