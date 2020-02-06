755408d2637589584ce94dc1f727fd8843ab1b89
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2020-02-06 22:05:44 +0400

    Update addbook.php

--- RP-31-book/form/addbook.php
+++ RP-31-book/form/addbook.php
@@ -1,5 +1,7 @@
 <?php
+//Подключенеие фона (background)
 include('../BG.php');
+//Работа с HTML формой
 echo '<form method="POST" action="' . '../process/process.php' . '">
         <input name="name" placeholder="Название книги">
         <input name="price" placeholder="Цена">
