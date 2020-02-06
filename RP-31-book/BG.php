ede78f6824825cb3a8d7aaee7843135eb49285d7
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2020-02-06 21:38:32 +0400

    Update BG.php

--- RP-31-book/BG.php
+++ RP-31-book/BG.php
@@ -1,5 +1,7 @@
 <?php
+//Подключение CSS файла
 echo '<link rel="stylesheet" href="../style/backvideo.css">';
+//HTML теги фона
 echo '<div class="fullscreen-bg">
     <video loop muted autoplay poster="" class="fullscreen-bg__video">
         <source src="../style/videoplayback.webm" type="video/webm">
