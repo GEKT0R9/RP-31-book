88fe0eb5d0037f4087610cb7f6eb8e941ff85116
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2020-02-06 22:21:37 +0400

    Update delete.php

--- RP-31-book/form/delete.php
+++ RP-31-book/form/delete.php
@@ -1,11 +1,17 @@
 <?php
+//Подключение БД
 require('../connect_bd.php');
+//Проверка на наличие данных в методе POST
 if (empty($_POST)) {
+    //Подключение фона (background)
     include('../BG.php');
+    //Начало работы с HTML формой
     echo '<form action="delete.php" method="post" accept-charset="UTF-8"><p>';
+    //Запись в переменную результат запроса БД
     $result = $dbc->query('SELECT books.title, authors.name, books.id as idB, authors.id as idA FROM books,authors,books_authors WHERE books_authors.id_books = books.id AND books_authors.id_author = authors.id');
+    //Преобразование результата запроса в массив
     $result = $result->fetchAll();
-
+    //Проверка массива на заполненость
     if (count($result) > 0) {
         echo '<select class="form-control" name="marka">' .
             '<option value="Выберите книгу" selected="" disabled="">Выберите книгу</option>';
@@ -16,14 +22,19 @@ if (empty($_POST)) {
     } else {
         echo '<p>В настоящее время книг нет.</p>';
     }
+    //Кнопка "Удалить"
     echo '<p><input id="butt" type="submit" value="Удалить"></p>';
+    //Кнопка "На главную"
     echo "<p><a href='../index.php'>На главную</a></p>";
     echo '</form>';
 
 } else {
+    //Преобразование строки с ID в массив ID
     $IDarr = explode('|',$_POST['marka']);
+    //Отправки запросов в БД
     $dbc->query('DELETE FROM books_authors WHERE id_author = '.$IDarr[0].' AND id_books = '.$IDarr[1].';');
     $dbc->query('DELETE FROM authors WHERE id = '.$IDarr[0].';');
     $dbc->query('DELETE FROM books WHERE id = '.$IDarr[1].';');
+    //Переадресация на главную
     header('Location: ../index.php');
 }
