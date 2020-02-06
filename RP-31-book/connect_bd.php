471a7ae23693ca0ea0e94e53a0e83f361ad6685a
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2020-02-06 21:42:02 +0400

    Update connect_bd.php

--- RP-31-book/connect_bd.php
+++ RP-31-book/connect_bd.php
@@ -1,14 +1,19 @@
 <?php
+//Пороверка блока try на ошибки
 try {
 //    $dbc = new PDO("mysql:host=localhost;dbname=bookshop;charset=utf8", "root", "");
 //    $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//Запись в переменную данные о базе данных
     $dsn = "mysql:host=localhost;dbname=book_shop;charset=utf8";
+//Запись в переменную параметры PDO
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
+//Подключение к БД через PDO
     $dbc = new PDO($dsn, "root", "", $opt);
 } catch (PDOException $err) {
+//Вывод ошибки
     echo "Ошибка: не удается подключиться: " . $err->getMessage();
 }
