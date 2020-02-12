7be136e4231b3abb21353e1d5a2e65f62568d1ba
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2020-02-12 15:41:08 +0400

    Add files via upload

---
+++ RP-31-book/form/editbook.php
@@ -0,0 +1,61 @@
+<?php
+
+//Подключение БД
+require('../connect_bd.php');
+//Проверка на наличие данных в методе POST
+if (empty($_POST)) {
+    //Подключение фона (background)
+    include('../BG.php');
+    //Начало работы с HTML формой
+    echo '<form action="editbook.php" method="post" accept-charset="UTF-8"><p>';
+    //Запись в переменную результат запроса БД
+    $result = $dbc->query('
+    SELECT
+    nado.title, GROUP_CONCAT(nado.name SEPARATOR \', \') as name, nado.idB as idB
+    FROM(
+        SELECT books.title as title, books.id as idB, authors.name as name 
+        FROM books,authors,books_authors
+        WHERE books_authors.id_books = books.id AND books_authors.id_author = authors.id
+    ) AS nado
+    GROUP BY nado.title, nado.idB
+    ');
+    //Преобразование результата запроса в массив
+    $result = $result->fetchAll();
+    //Проверка массива на заполненость
+    if (count($result) > 0) {
+        echo '<select class="form-control" name="marka">' .
+            '<option value="Выберите книгу" selected="" disabled="">Выберите книгу</option>';
+        for ($i = 0; $i < count($result); ++$i) {
+            echo '<option value="' . $result[$i]['idB'] . '">' . $result[$i]['title'] . ' - ' . $result[$i]['name'] . '</option>';
+        }
+        echo '</select></p>';
+    } else {
+        echo '<p>В настоящее время книг нет.</p>';
+    }
+    //Кнопка "Удалить"
+    echo '<p><input id="butt" type="submit" value="Редактировать"></p>';
+    //Кнопка "На главную"
+    echo "<p><a href='../index.php'>На главную</a></p>";
+    echo '</form>';
+
+} else {
+    //Подключение фона (background)
+    include('../BG.php');
+    //Запись в переменную результат запроса БД
+    $result = $dbc->query('SELECT authors.name FROM books_authors, authors WHERE authors.id = books_authors.id_author AND id_books = ' . $_POST['marka']);
+    //Преобразование результата запроса в переменную
+    $resultA = $result->fetchAll();
+    $result = $dbc->query('SELECT title,price FROM books WHERE id = ' . $_POST['marka']);
+    //Преобразование результата запроса в переменную
+    $result = $result->fetchAll()[0];
+    echo '<form method="POST" action="' . '../process/process_editbook.php' . '">
+        Название книги: <input name="name" placeholder="Название книги" value="' . $result['title'] . '"><br>
+        Цена:<input name="price" placeholder="Цена" value="' . $result['price'] . '" ><br>
+        <input name="idB" value="'.$_POST['marka'].'|'.count($resultA).'" style="display: none;">';
+    for ($i = 0; $i < count($resultA); ++$i) {
+        echo 'Автор №'.($i+1).'<input name="name_a' . $i . '" placeholder="Имя автора" value="' . $resultA[$i]['name'] . '"><br>';
+    }
+    echo '<br><button type="submit" class="but">Изменить</button>
+          <p><a href=\'../index.php\'>На главную</a></p>
+          </form>';
+}
---
+++ RP-31-book/process/process_editbook.php
@@ -0,0 +1,81 @@
+<?php
+var_dump($_POST);
+
+//Функция вывода ошибки ввода
+function fail($str, $id = true)
+{
+    //Подключение фона(background)
+    include('../BG.php');
+    echo '<title>Ошибка ввода</title>';
+    if ($id) {
+        echo "<p>Пожалуйста, укажите $str.</p>";
+    } else {
+        echo "<p>$str.</p>";
+    }
+    echo "<p><a href='../form/editbook.php'>Заполнить заново</a></p>";
+    exit();
+}
+
+//Проверка на заполненость метода POST
+if (isset($_POST)) {
+
+    //Проверки
+    if (!empty(trim($_POST['name']))) {
+        $name = trim(addslashes($_POST['name']));
+    } else {
+        fail('название книги');
+    }
+
+    if (is_numeric(trim($_POST['price']))) {
+        if (!empty(trim($_POST['price']))) {
+            $price = trim(addslashes($_POST['price']));
+        } else {
+            fail('цену книги');
+        }
+    } else {
+        fail('Неверный формат поля: "цена"', false);
+    }
+    $IDarr = explode('|',$_POST['idB']);
+    $idB = $IDarr[0];
+    $count = $IDarr[1];
+    $name_a = array();
+    for ($i = 0;$i<$count;++$i){
+        if (!empty(trim($_POST['name_a'.$i.'']))) {
+            $name_a[$i] = trim(addslashes($_POST['name_a'.$i.'']));
+        } else {
+            fail('Укажите автора', false);
+        }
+    }
+    //Подключение БД
+    //Запись в переменную SQL запроса
+    $sql = "INSERT INTO books (title, price) VALUES ('$name', '$price')";
+    //Отправка запроса БД
+    $dbc->query($sql);
+    //Отправка запроса БД и запись результата в переменную
+    $sql = $dbc->query("SELECT id FROM books");
+    //Преобразование результата запроса в массив
+    $sql = $sql->fetchAll();
+    //Запись в одномерный массив последней строки двумерного
+    $sql = $sql[count($sql) - 1];
+    //Запись в переменную ID книги
+    $idB = $sql['id'];
+
+    //Запись в переменную SQL запроса
+    $sql = "INSERT INTO authors (name) VALUES ('$name_a')";
+    //Отправка запроса БД
+    $dbc->query($sql);
+    //Отправка запроса БД и запись результата в переменную
+    $sql = $dbc->query("SELECT id FROM authors");
+    //Преобразование результата запроса в двумерный массив
+    $sql = $sql->fetchAll();
+    //Запись в одномерный массив последней строки двумерного
+    $sql = $sql[count($sql) - 1];
+    //Запись в переменную ID автора
+    $idA = $sql['id'];
+    //Запись в переменную SQL запроса
+    $sql = "INSERT INTO books_authors (id_author, id_books) VALUES ('$idA','$idB')";
+    //Отправка запроса БД
+    $dbc->query($sql);
+}
+//Перессылка на главную
+header('Location: ../index.php');
